011f52e3c137cfcdbb94ef6c123bb44c56d40160
Aoi <ht.so.ik0102@gmail.com>
2021-07-12 20:15:09 +0700

    add some function

--- dTrustToken.sol
+++ dTrustToken.sol
@@ -1,10 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.8;
 
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/GSN/Context.sol";
 
-contract DTrustToken {
+contract DTrustToken is Ownable, Pausable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
+    string public constant name;
+    string public constant symbol;
+    uint8 public constant decimals;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+
+    uint256 totalSupply_;
+
+    constructor(uint256 total) public {
+        totalSupply_ = total;
+        balances[msg.sender] = _totalSupply;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint256 numTokens)
+        public
+        returns (bool)
+    {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint256 numTokens)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate)
+        public
+        view
+        returns (uint256)
+    {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint256 numTokens
+    ) public returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[from][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        Transfer(owner, buyer, numTokens);
+        return true;
+    }
 }
