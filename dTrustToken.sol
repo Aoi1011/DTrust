fb5fa25282c7d0fca50868a49a0569f29b907bb6
Aoi <ht.so.ik0102@gmail.com>
2021-07-12 18:43:43 +0700

    set up import

--- dTrustToken.sol
+++ dTrustToken.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.8;
 
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
+
 contract DTrustToken {
-    
+
 }
