860e44cc5dceb44a47e9a4b19277afc8dc2b9382
Aoi <ht.so.ik0102@gmail.com>
2021-07-25 00:34:42 +0700

    add Address.sol and SafeERC20.sol

--- contracts/DTRUST.sol
+++ contracts/DTRUST.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 // import "../node_modules/@nomiclabs/buidler/console.sol"; // advance debugging
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol"; // --> safe ERC1155 internals
 import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/security/Pausable.sol";
 
-import "./SafeMath.sol";
-import "./stringUtils.sol";
+import "./libraries/SafeMath.sol";
+import "./libraries/stringUtils.sol";
+import "./libraries/SafeERC20.sol";
 
 contract DTRUSTs {
     DTRUST[] public deployedDTRUSTs;
