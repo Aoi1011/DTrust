b58058fdbdf005aa0f9513521cc602cdb5538024
Aoi <ht.so.ik0102@gmail.com>
2021-07-26 19:33:20 +0700

    move control key function

--- contracts/ControlKey.sol
+++ contracts/ControlKey.sol
@@ -2,5 +2,89 @@
 pragma solidity ^0.8.0;
 
 contract ControlKey {
-    
+    struct StructControlKey {
+        string privateKey;
+        address[] settlors;
+        address[] beneficiaries;
+        address[] trustees;
+        bool usable;
+        bool burnable;
+    }
+    uint256 public numControlKey;
+
+    mapping(uint256 => StructControlKey) controlKeys;
+
+    function generateControlKey(
+        string memory _privateKey,
+        address[] memory _settlors,
+        address[] memory _beneficiaries,
+        address[] memory _trustees
+    ) public returns (uint256 controlKeyId) {
+        controlKeyId = numControlKey++;
+        controlKeys[controlKeyId] = StructControlKey({
+            privateKey: _privateKey,
+            settlors: _settlors,
+            beneficiaries: _beneficiaries,
+            trustees: _trustees,
+            usable: false,
+            burnable: false
+        });
+    }
+
+    function getControlKey(uint256 _controlKeyId)
+        public
+        view
+        returns (StructControlKey memory existControlKey)
+    {
+        require(_controlKeyId >= 0, "Control Key must be more than 0");
+        require(_controlKeyId <= numControlKey, "ControlKey does not exist.");
+        return controlKeys[_controlKeyId];
+    }
+
+    function handleUsableControlKey(uint256 _controlKeyId) public {
+        require(_controlKeyId >= 0, "Control Key must be more than 0");
+        require(
+            _controlKeyId <= numControlKey,
+            "ControlKey must be less than total"
+        );
+        StructControlKey memory existControlKey = controlKeys[_controlKeyId];
+        controlKeys[_controlKeyId] = StructControlKey({
+            privateKey: existControlKey.privateKey,
+            settlors: existControlKey.settlors,
+            beneficiaries: existControlKey.beneficiaries,
+            trustees: existControlKey.trustees,
+            usable: !existControlKey.usable,
+            burnable: existControlKey.burnable
+        });
+    }
+
+    function handleBurnableControlKey(uint256 _controlKeyId) public {
+        require(_controlKeyId >= 0, "Control Key must be more than 0");
+        require(
+            _controlKeyId <= numControlKey,
+            "ControlKey must be less than total"
+        );
+        StructControlKey memory existControlKey = controlKeys[_controlKeyId];
+        controlKeys[_controlKeyId] = StructControlKey({
+            privateKey: existControlKey.privateKey,
+            settlors: existControlKey.settlors,
+            beneficiaries: existControlKey.beneficiaries,
+            trustees: existControlKey.trustees,
+            usable: existControlKey.usable,
+            burnable: !existControlKey.burnable
+        });
+    }
+
+    function destroyControlKey(uint256 _controlKeyId) public {
+        require(_controlKeyId >= 0, "Control Key must be more than 0");
+        require(
+            _controlKeyId <= numControlKey,
+            "ControlKey must be less than total"
+        );
+
+        StructControlKey memory existControlKey = controlKeys[_controlKeyId];
+        require(existControlKey.burnable, "Can not destroy.");
+
+        delete controlKeys[_controlKeyId];
+    }
 }
--- contracts/DTRUST.sol
+++ contracts/DTRUST.sol
@@ -33,19 +33,9 @@ contract DTRUST is ERC1155, Ownable, Pausable {
         string tokenName; // PrToekn, DToken
     }
 
-    // struct ControlKey {
-    //     string privateKey;
-    //     address[] settlors;
-    //     address[] beneficiaries;
-    //     address[] trustees;
-    //     bool usable;
-    //     bool burnable;
-    // }
-
     // uint256 private _AnualFeeTotal;
     // uint256 public percent = 25;
     // uint256 public _SemiAnnualFee = percent / 100; // it can be updated later  percent
-    // uint256 public numControlKey;
     uint256[] public tokenIds;
     address payable public manager;
     address payable public settlor;
@@ -55,7 +45,6 @@ contract DTRUST is ERC1155, Ownable, Pausable {
     string private _uri;
 
     // storage//////////////////////////
-    // mapping(uint256 => ControlKey) controlKeys;
     mapping(uint256 => TokenType) public tokenType; // id -> tokenType
     mapping(uint256 => uint256) public tokenSupply; // id -> tokensupply
     mapping(uint256 => uint256) public tokenPrices; // id -> tokenPrice
@@ -318,79 +307,5 @@ contract DTRUST is ERC1155, Ownable, Pausable {
     //     tokenSupply[_id] = tokenSupply[_id].add(semiAnnualFee);
 
     //     _AnualFeeTotal.add(semiAnnualFee);
-    // }
-
-    // function generateControlKey(
-    //     string memory _privateKey,
-    //     address[] memory _settlors,
-    //     address[] memory _beneficiaries,
-    //     address[] memory _trustees
-    // ) public returns (uint256 controlKeyId) {
-    //     controlKeyId = numControlKey++;
-    //     controlKeys[controlKeyId] = ControlKey({
-    //         privateKey: _privateKey,
-    //         settlors: _settlors,
-    //         beneficiaries: _beneficiaries,
-    //         trustees: _trustees,
-    //         usable: false,
-    //         burnable: false
-    //     });
-    // }
-
-    // function getControlKey(uint256 _controlKeyId)
-    //     public
-    //     view
-    //     returns (ControlKey memory existControlKey)
-    // {
-    //     require(_controlKeyId >= 0, "Control Key must be more than 0");
-    //     require(_controlKeyId <= numControlKey, "ControlKey does not exist.");
-    //     return controlKeys[_controlKeyId];
-    // }
-
-    // function handleUsableControlKey(uint256 _controlKeyId) public {
-    //     require(_controlKeyId >= 0, "Control Key must be more than 0");
-    //     require(
-    //         _controlKeyId <= numControlKey,
-    //         "ControlKey must be less than total"
-    //     );
-    //     ControlKey memory existControlKey = controlKeys[_controlKeyId];
-    //     controlKeys[_controlKeyId] = ControlKey({
-    //         privateKey: existControlKey.privateKey,
-    //         settlors: existControlKey.settlors,
-    //         beneficiaries: existControlKey.beneficiaries,
-    //         trustees: existControlKey.trustees,
-    //         usable: !existControlKey.usable,
-    //         burnable: existControlKey.burnable
-    //     });
-    // }
-
-    // function handleBurnableControlKey(uint256 _controlKeyId) public {
-    //     require(_controlKeyId >= 0, "Control Key must be more than 0");
-    //     require(
-    //         _controlKeyId <= numControlKey,
-    //         "ControlKey must be less than total"
-    //     );
-    //     ControlKey memory existControlKey = controlKeys[_controlKeyId];
-    //     controlKeys[_controlKeyId] = ControlKey({
-    //         privateKey: existControlKey.privateKey,
-    //         settlors: existControlKey.settlors,
-    //         beneficiaries: existControlKey.beneficiaries,
-    //         trustees: existControlKey.trustees,
-    //         usable: existControlKey.usable,
-    //         burnable: !existControlKey.burnable
-    //     });
-    // }
-
-    // function destroyControlKey(uint256 _controlKeyId) public {
-    //     require(_controlKeyId >= 0, "Control Key must be more than 0");
-    //     require(
-    //         _controlKeyId <= numControlKey,
-    //         "ControlKey must be less than total"
-    //     );
-
-    //     ControlKey memory existControlKey = controlKeys[_controlKeyId];
-    //     require(existControlKey.burnable, "Can not destroy.");
-
-    //     delete controlKeys[_controlKeyId];
     // }
 }
