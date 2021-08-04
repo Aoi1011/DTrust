e3125f72545168bca05b7fd2d0160479c88c2fc0
Aoi <ht.so.ik0102@gmail.com>
2021-08-04 09:16:34 +0700

    change uint2str

--- contracts/DTRUST.sol
+++ contracts/DTRUST.sol
@@ -246,9 +246,12 @@ contract DTRUST is ERC1155 {
         }
 
         bytes memory bstr = new bytes(len);
-        uint256 k = len - 1;
+        uint256 k = len;
         while (_i != 0) {
-            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
             _i /= 10;
         }
 
