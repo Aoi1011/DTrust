ffcd83e0d1ba338d7fb4b6f41fb67771f1bda4f0
Aoi <ht.so.ik0102@gmail.com>
2021-08-13 20:42:38 +0700

    add comment in 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,9 @@ const ControKey = artifacts.require("ControlKey");
 
 module.exports = async function (deployer, network, accounts) {
 
-    deployer.deploy(DTRUST, "DTRUSTAdmin", "DTA", "DTRUSTAdmin", accounts[0], accounts[0], accounts[0], accounts[0]).then(() => {
-        console.log(DTRUST.address);
-    });
+    // deployer.deploy(DTRUST, "DTRUSTAdmin", "DTA", "DTRUSTAdmin", accounts[0], accounts[0], accounts[0], accounts[0]).then(() => {
+    //     console.log(DTRUST.address);
+    // });
 
     deployer.deploy(DTRUSTFactory).then(() => {
         console.log(DTRUSTFactory.address);
