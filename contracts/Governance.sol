0b54acd9f9a6e15f0b7d1e1f8baf21c93667cc33
Aoi <ht.so.ik0102@gmail.com>
2021-08-21 11:27:14 +0700

    create new register function

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Governance {
     IERC20 public DTtoken;
+    address[] voters;
     // voter => deposit
     mapping(address => uint256) public deposits;
 
@@ -15,6 +16,11 @@ contract Governance {
         DTtoken = _DTtoken;
     }
 
+    function registerVoter(address _newVoter) external {
+        require(_newVoter != address(0));
+        voters.push(_newVoter);
+    }
+
     function deposit(uint256 _amount) external {
         deposits[msg.sender] += _amount;
         DTtoken.transferFrom(msg.sender, address(this), _amount);
@@ -23,5 +29,9 @@ contract Governance {
     function withdraw(uint256 _amount) external {
         deposits[msg.sender] -= deposits[msg.sender];
         DTtoken.transfer(msg.sender, _amount);
+    }
+
+    function splitAnnualFee(uint256 _amount) external {
+        uint256 totalOfDTtoken = DTtoken.totalSupply();
     }
 }
