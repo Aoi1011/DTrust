8e4c1f1a68ca879091447ebbf4632cc2138c3f18
Aoi <ht.so.ik0102@gmail.com>
2021-09-19 15:56:15 +0700

    change migration file

--- contracts/DTRUST.sol
+++ contracts/DTRUST.sol
@@ -59,9 +59,9 @@ contract DTRUST is ERC1155, KeeperCompatibleInterface {
     address payable public immutable trustee;
     address public immutable beneficiary;
     address public immutable promoter;
-    string public immutable dTrustUri;
+    string public dTrustUri;
     bool public immutable hasPromoter;
-    Subscription private immutable subscription;
+    Subscription private subscription;
 
     mapping(uint256 => bool) public existToken;
     mapping(uint256 => ERC20TokenAsset) public erc20TokenAssets;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,13 +6,19 @@ const PRtoken = artifacts.require("PRtoken");
 
 module.exports = async function (deployer, network, accounts) {
 
-    const manager = "0x1Bb0ebE711a73347ae2F2A765A06AfAfB14c9A93";
+    // const manager = "0x1Bb0ebE711a73347ae2F2A765A06AfAfB14c9A93";
+    const manager = "0x49F67373f007aD8248f3ECDBA76f17b3e0DE3141";
 
     deployer.deploy(Governance)
-        .then((result) => {
-            deployer.deploy(DTtoken, manager, result.address);
-            result.registerDTtoken(DTtoken.address)
-            deployer.deploy(DTRUSTFactory, result.address);
+        .then((governanceResult) => {
+
+            deployer.deploy(DTtoken, manager, governanceResult.address)
+                .then((dttokenResult) => {
+
+                    governanceResult.registerDTtoken(dttokenResult.address)
+                });
+
+            deployer.deploy(DTRUSTFactory, governanceResult.address);
         })
         .then(() => deployer.deploy(PRtoken, manager))
         .then(() => deployer.deploy(ControlKey));
